4cf391b2648c8ece3be3ef8a306975f160bc2179
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-10-22 15:55:00 +0530

    dashboard updated

--- CarRental_BackEnd/src/main/java/lk/ijse/carRent/WebAppInitializer.java
+++ CarRental_BackEnd/src/main/java/lk/ijse/carRent/WebAppInitializer.java
@@ -1,4 +1,19 @@
 package lk.ijse.carRent;
 
-public class WebAppInitializer {
+import lk.ijse.carRent.config.WebAppConfig;
+import lk.ijse.carRent.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
---
+++ CarRental_BackEnd/src/main/java/lk/ijse/carRent/config/JPAConfig.java
@@ -0,0 +1,4 @@
+package lk.ijse.carRent.config;
+
+public class JPAConfig {
+}
