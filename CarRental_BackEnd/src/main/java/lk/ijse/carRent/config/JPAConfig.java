55ae03ce54e3403b7312df85f55415051ec57dc6
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-10-24 17:21:50 +0530

    properties added

--- CarRental_BackEnd/src/main/java/lk/ijse/carRent/config/JPAConfig.java
+++ CarRental_BackEnd/src/main/java/lk/ijse/carRent/config/JPAConfig.java
@@ -1,4 +1,14 @@
 package lk.ijse.carRent.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "lk.ijse.carRent.repo")
+@PropertySource("classpath:application.properties")
 public class JPAConfig {
+
 }
